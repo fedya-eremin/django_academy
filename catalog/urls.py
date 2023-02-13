a5675144219701c09b6e5466f9b35f9571ba66b1
sunfireaegis <henry_morgan06@mail.com>
2023-02-13 08:15:49 +0300

    task 2.1: renamed tests

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.item_list), path("<int:key>/", views.item_detail)]
+urlpatterns = [
+    path("", views.item_list),
+    path("<int:key>/", views.item_detail),
+]
