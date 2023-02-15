c9f940a12b67496202b54836d60db7cbd7f59e72
sunfireaegis <henry_morgan06@mail.com>
2023-02-15 20:37:44 +0300

    2.3 added regexp to middleware & redesigned tests

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,27 +1,9 @@
-from django.test import Client, TestCase
-
-import environ
+from django.test import Client, TestCase, modify_settings
 
 
 class StaticUrlTests(TestCase):
-    def test_homepage_endpoint(self):
-        response = Client().get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_coffee_endpoint(self):
-        response = Client().get("/coffee")
-        with self.subTest("teapot"):
-            # self.assertEqual(response.content, "I'm a teapot: /coffee")
-            self.assertEqual(response.status_code, 418)
-
-        with self.subTest("content"):
-            self.assertEqual(response.content.decode("utf-8"), "Я - чайник!")
-
-    def test_reverse_ru_middleware(self):
-        """tests my middleware"""
-        env = environ.Env(ENABLE_REVERSE_RU_MIDDLEWARE=(bool, False))
-        is_enabled = env("ENABLE_REVERSE_RU_MIDDLEWARE")
-        tests = {
+    def setUp(self):
+        self.tests = {
             "человек на луне": "<body>кеволеч ан енул</body>",
             "яндекс браузер": "<body>скедня резуарб</body>",
             "google": "<body>google</body>",
@@ -32,25 +14,49 @@ class StaticUrlTests(TestCase):
             True: "<body>True</body>",
             None: "<body>None</body>",
         }
-        if is_enabled is True:  # if middleware is enabled
-            for test in tests:
-                with self.subTest(test):
-                    for i in range(9):
-                        response = self.client.get(f"/test/{test}")
-                        self.assertEqual(
-                            response.content.decode("utf-8"),
-                            f"<body>{test}</body>",
-                        )
+
+    def test_homepage_endpoint(self):
+        response = Client().get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_coffee_endpoint(self):
+        response = Client().get("/coffee")
+        with self.subTest("teapot"):
+            self.assertEqual(response.status_code, 418)
+
+        with self.subTest("content"):
+            self.assertEqual(response.content.decode("utf-8"), "Я - чайник!")
+
+    @modify_settings(
+        MIDDLEWARE={
+            "append": "myserver.middleware.ReverseRuMiddleware",
+        }
+    )
+    def test_reverse_ru_middleware_on(self):
+        for test in self.tests:
+            with self.subTest(test):
+                for i in range(9):
                     response = self.client.get(f"/test/{test}")
                     self.assertEqual(
-                        response.content.decode("utf-8"), tests[test]
+                        response.content.decode("utf-8"),
+                        f"<body>{test}</body>",
                     )
-        else:  # otherwise
-            for test in tests:
-                with self.subTest(test):
-                    for i in range(10):
-                        response = self.client.get(f"/test/{test}")
-                        self.assertEqual(
-                            response.content.decode("utf-8"),
-                            f"<body>{test}</body>",
-                        )
+                response = self.client.get(f"/test/{test}")
+                self.assertEqual(
+                    response.content.decode("utf-8"), self.tests[test]
+                )
+
+    @modify_settings(
+        MIDDLEWARE={
+            "remove": "myserver.middleware.ReverseRuMiddleware",
+        }
+    )
+    def test_reverse_ru_middleware_off(self):
+        for test in self.tests:
+            with self.subTest(test):
+                for i in range(10):
+                    response = self.client.get(f"/test/{test}")
+                    self.assertEqual(
+                        response.content.decode("utf-8"),
+                        f"<body>{test}</body>",
+                    )
--- myserver/middleware.py
+++ myserver/middleware.py
@@ -1,3 +1,6 @@
+import re
+
+
 class ReverseRuMiddleware:
     def __init__(self, get_response):
         self.count = 1
@@ -5,18 +8,20 @@ class ReverseRuMiddleware:
 
     def __call__(self, request):
         """reverses russian words in each tenth response"""
-        alphabet = (
-            "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-        )
         response = self.get_response(request)
         content = list(response.content.decode("utf-8"))
         begin = -1
         if not self.count % 10:
             for i in range(len(content) - 1):
-                if begin == -1 and content[i] in alphabet:
+                if (
+                    begin == -1
+                    and re.search(r"[а-яА-ЯёЁ]", content[i]) is not None
+                ):
                     begin = i
-                if begin != -1 and content[i + 1] not in alphabet:
+                if (
+                    begin != -1
+                    and re.search(r"[а-яА-ЯёЁ]", content[i + 1]) is None
+                ):
                     content[begin : i + 1] = reversed(content[begin : i + 1])
                     begin = -1
             response.content = "".join(content)
