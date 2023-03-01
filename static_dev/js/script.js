930c375df7a97b8b3dd2fdd29b9a7ba1729dacb0
fedya-eremin <qwertier06@gmail.com>
2023-03-01 14:36:23 +0300

    4.3 added time in footer and dymamic ru/en translations

--- static_dev/js/script.js
+++ static_dev/js/script.js
@@ -1,10 +1,7 @@
-
 const a = document.getElementsByTagName("footer")[0]
-	.appendChild(document.createElement("p"));
-currentTime = () => {
-	a.innerHTML = Date(Date(Intl.DateTimeFormat().resolvedOptions().timeZone));
+	.appendChild(document.createElement("div"));
+const currentTime = () => {
+	const date = new Intl.DateTimeFormat().resolvedOptions().timeZone;
+	a.innerHTML = Date(date);
 }
-
 setInterval(currentTime, 1000);
-
-
