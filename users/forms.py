b84906d5e6cc9e8ed2e3da91867ac4ca076e48b2
fedya-eremin <qwertier06@gmail.com>
2023-03-23 22:44:23 +0300

    finally,

--- users/forms.py
+++ users/forms.py
@@ -47,7 +47,7 @@ class MyLoginForm(Form):
 class UserForm(BootstrapClassesMixin):
     class Meta:
         model = User
-        fields = ("username", "email")
+        fields = ("username", "email", "first_name", "last_name")
 
 
 class ProfileForm(BootstrapClassesMixin):
